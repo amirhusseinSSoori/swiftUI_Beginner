c1b1fb2201f54b8c5ec9e784e478226f58c6828e
Amirhussein soori <amirhusseinsppro75@gmail.com>
2022-03-31 04:31:16 +0430

    Initial Commit

---
+++ Concept_swiftUi/Concept_swiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Concept_swiftUiApp.swift
+//  Concept_swiftUi
+//
+//  Created by Admin on 3/31/22.
+//
+
+import SwiftUI
+
+@main
+struct Concept_swiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Concept_swiftUi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Concept_swiftUi
+//
+//  Created by Admin on 3/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
